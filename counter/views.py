ad0226c3588319a4bf84a896cbbb89d027aa190a
Lucas Lima Batista <lucaslimabatista.eng@gmail.com>
2023-10-28 11:38:40 -0300

    Cards de alimentos

--- counter/views.py
+++ counter/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'counter/home.html')
+    return render(request, 'counter/pages/home.html')
 
 # Create your views here.
